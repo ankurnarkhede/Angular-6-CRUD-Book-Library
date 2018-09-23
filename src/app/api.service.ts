92dfa985e365603e9e08989f55f24c772b4f5546
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-09-23 08:27:59 +0530

    Added materialize

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-const apiUrl = "/api";
+const apiUrl = "localhost:3000/api";
 
 
 @Injectable({
